e2b468dcc1bbb2cd8a659ebeb57e43abea6563fe
Damian Jason Lapidge <greydamian@users.noreply.github.com>
2015-02-06 23:30:09 +0000

    Improved build.bat

--- build.bat
+++ build.bat
@@ -5,24 +5,37 @@ rem
 rem The contents of this file are subject to the terms and conditions defined 
 rem within the file LICENSE.txt, located within this project's root directory.
 
+rem compiler, archiver, and runtime commands (could specify absolute paths)
+set JAVAC=javac
+set JAR=jar
+set JAVA=java
+
+rem output directory for class files
+set CLASSDIR=.\bin\classes
+
+rem application entry point
+set MAINCLASS=org.greydamian.netbatjava.Netbat
+
 set SRC=.\src\org\greydamian\netbatjava\Netbat.java
 set OUT=.\bin\netbat.jar
 
-set MAINCLASS=org.greydamian.netbatjava.Netbat
+rem extract bin directory from output path (dirname %OUT%)
+for %%f in (%OUT%) do set BINDIR=%%~dpf
 
-rem create output directory
-mkdir .\bin\classes
+rem create output directories
+mkdir %BINDIR%
+mkdir %CLASSDIR%
 
 rem compile java source code
-javac -d .\bin\classes %SRC%
+%JAVAC% -d %CLASSDIR% %SRC%
 
 rem create executable jar file
-jar cfe %OUT% %MAINCLASS% -C .\bin\classes\ .
+%JAR% cfe %OUT% %MAINCLASS% -C %CLASSDIR% .
 
 rem remove compiled class files
-rmdir /q /s .\bin\classes
+rmdir /q /s %CLASSDIR%
 
 rem create execution script
 echo @echo off >.\bin\netbat-java.bat
 echo. >>.\bin\netbat-java.bat
-echo java -jar %%~dp0.\netbat.jar %%* >>.\bin\netbat-java.bat
+echo %JAVA% -jar %%~dp0.\netbat.jar %%* >>.\bin\netbat-java.bat
